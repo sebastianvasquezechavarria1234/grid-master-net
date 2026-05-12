a24d35f7e94734b13e5097aadea4ae18bbf362b2
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-05-12 15:24:39 -0500

    Initial commit

---
+++ CrudConDataTables/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudConDataTables.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
